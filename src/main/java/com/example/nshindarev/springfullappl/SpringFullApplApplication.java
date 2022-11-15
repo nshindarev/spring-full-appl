35be2d2e7797b9ab9d8e7e5ee02dd7b5cad1c4dc
Shindarev Nikita <nshindarev@gmail.com>
2022-11-15 16:22:17 +0300

    bootstrap project via spring initilizr

---
+++ src/main/java/com/example/nshindarev/springfullappl/SpringFullApplApplication.java
@@ -0,0 +1,13 @@
+package com.example.nshindarev.springfullappl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullApplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullApplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nshindarev/springfullappl/SpringFullApplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nshindarev.springfullappl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullApplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
